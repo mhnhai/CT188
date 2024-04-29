9a1d69d85b8e1bf6f59fa9ac22d7f398c7dd627f
mhnhai <maihangocha@gmail.com>
2024-04-29 20:32:00 +0700

    Update Admin.js

--- JavaScripts/Admin.js
+++ JavaScripts/Admin.js
@@ -7,21 +7,3 @@ document.addEventListener('DOMContentLoaded', function() {
     handleActiveTabs();
 });
 
-     if (contentId === 'products') {
-         // Hiển thị nội dung danh mục sản phẩm 
-        displayClothes();
-    }
-}
-
-function displayClothes() {
-    var clothesTable = '<tbody id="product-tbody">';
-
-    // Lấy dữ liệu từ localStorage và chuyển đổi thành danh sach
-    var storedData = localStorage.getItem('clothesData');
-    var parsedList = JSON.parse(storedData);
-    parsedList.forEach(function(clothes) {
-        clothesTable += '<tr><td>' + clothes.id + '</td><td>' + clothes.name + '</td><td>' + clothes.category + '</td><td id="soluong">' + clothes.soluong + '</td></tr>';
-    });
-    clothesTable += '</tbody>'; 
-    document.getElementById('product-tbody').innerHTML = clothesTable;
-}
