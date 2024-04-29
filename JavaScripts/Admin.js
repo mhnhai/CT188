fa3e24376a6dcc7879aa67035ffea954cb1c62fe
mhnhai <maihangocha@gmail.com>
2024-04-29 20:31:54 +0700

    update

--- JavaScripts/Admin.js
+++ JavaScripts/Admin.js
@@ -1,32 +1,11 @@
-// Danh sách dữ liệu quần áo
-var clothesData = [
-    { id: 1, name: 'Áo sơ mi', category: 'Áo', soluong: 10},
-    { id: 2, name: 'Quần jeans', category: 'Quần', soluong: 10},
-    { id: 3, name: 'Váy dài', category: 'Váy', soluong: 10},
-    { id: 4, name: 'Áo len', category: 'Áo', soluong: 10},
-    { id: 5, name: 'Quần short', category: 'Quần', soluong: 10},
-    { id: 6, name: 'Áo khoác', category: 'Áo', soluong: 10},
-    { id: 7, name: 'Áo thun', category: 'Áo', soluong: 10},
-    { id: 8, name: 'Quần âu', category: 'Quần', soluong: 10}, 
-];
+import { handleActiveTabs, showContent } from '../JavaScripts/AdminEventHandle.js';
 
-// Kiểm tra xem Local Storage có hỗ trợ không
-if (typeof(Storage) !== "undefined") {
-    // Nếu đã có dữ liệu sản phẩm trong Local Storage, lấy dữ liệu từ Local Storage
-    if (localStorage.getItem("clothesData")) {
-        clothesData = JSON.parse(localStorage.getItem("clothesData"));
-    } else {
-        // Nếu chưa có dữ liệu, lưu dữ liệu vào Local Storage
-        localStorage.setItem("clothesData", JSON.stringify(clothesData));
-    }
-}
+// importDataToLocalStorage();
+importDataToLocalStorage();
 
-function showContent(contentId) {
-    var contents = document.getElementsByClassName('content');
-    for (var i = 0; i < contents.length; i++) {
-        contents[i].style.display = 'none';
-    }
-    document.getElementById(contentId).style.display = 'block';
+document.addEventListener('DOMContentLoaded', function() {
+    handleActiveTabs();
+});
 
      if (contentId === 'products') {
          // Hiển thị nội dung danh mục sản phẩm 
--- JavaScripts/EnterProductsController.js
+++ JavaScripts/EnterProductsController.js
@@ -1,1 +1,7 @@
-import { productTypes, products, brands } from "./MainDB";
+window.addProduct = function(product) {
+  loadDataFromLocalStorage();
+  const newProductId = window.products.length > 0 ? window.products[window.products.length - 1].id + 1 : 1;
+  product.id = newProductId;
+  window.products.push(product);
+  importDataToLocalStorage();
+};
