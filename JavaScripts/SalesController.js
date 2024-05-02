fa8b839f92b14c003e87e64fa652f35cc226a90a
AurorantZ <zaloveliberty@gmail.com>
2024-05-03 00:13:47 +0700

    Admin, details

--- JavaScripts/SalesController.js
+++ JavaScripts/SalesController.js
@@ -1,28 +1,50 @@
 window.getBills = function () {
-    loadDataFromLocalStorage();
-    return bills;
+  loadDataFromLocalStorage();
+  return bills;
 }
 
 window.getBill = function (billID) {
-    loadDataFromLocalStorage();
-    return bills.find(bill => bill.id === billID);
+  loadDataFromLocalStorage();
+  return bills.find(bill => bill.id === billID);
 }
 
 window.getPurchasedProducts = function (billID) {
-    loadDataFromLocalStorage();
-    return purchasedProducts.filter(product => product.billID === billID);
+  loadDataFromLocalStorage();
+  return purchasedProducts.filter(product => product.billID === billID);
 }
 
 window.getBillValue = function (billID) {
-    const purchasedProductsInBill = purchasedProducts.filter(purchasedProduct => purchasedProduct.billID === billID);
-  
-    let totalValue = 0;
-    for (const purchasedProduct of purchasedProductsInBill) {
-      const product = products.find(product => product.id === purchasedProduct.productID);
-      if (product) {
-        totalValue += product.price * purchasedProduct.amount;
-      }
+  const purchasedProductsInBill = purchasedProducts.filter(purchasedProduct => purchasedProduct.billID === billID);
+
+  let totalValue = 0;
+  for (const purchasedProduct of purchasedProductsInBill) {
+    const product = products.find(product => product.id === purchasedProduct.productID);
+    if (product) {
+      totalValue += product.price * purchasedProduct.amount;
     }
+  }
+  
+  return totalValue;
+}
+
+window.calculateRevenue = function() {
+let revenue = 0;
+
+window.purchasedProducts.forEach(purchasedProduct => {
+    const product = window.products.find(product => product.id === purchasedProduct.productID);
     
-    return totalValue;
+    if (product && product.price && purchasedProduct.amount) {
+        revenue += product.price * purchasedProduct.amount;
+    }
+});
+
+return revenue;
+}
+
+window.calculateTotalPurchasedQuantity = function() {
+let totalPurchasedQuantity = 0;
+window.purchasedProducts.forEach(purchasedProduct => {
+    totalPurchasedQuantity += purchasedProduct.amount;
+});
+return totalPurchasedQuantity;
 }
