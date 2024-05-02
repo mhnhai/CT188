61df30be15c6e038b129deb75af91b2a2ff3719c
mhnhai <maihangocha@gmail.com>
2024-05-02 21:15:23 +0700

    update

--- JavaScripts/EnterProductsController.js
+++ JavaScripts/EnterProductsController.js
@@ -1,7 +1,8 @@
-window.addProduct = function(product) {
+window.addProduct = function(frm) {
   loadDataFromLocalStorage();
-  const newProductId = window.products.length > 0 ? window.products[window.products.length - 1].id + 1 : 1;
-  product.id = newProductId;
-  window.products.push(product);
+  // const newProductId = window.products.length > 0 ? window.products[window.products.length - 1].id + 1 : 1;
+  const newProductId = window.products.length +1;
+  var obj = {"id":newProductId, "productName":frm.name.value, "image":frm.image.result,"price":frm.price.value,"category":frm.category.value,"amount":frm.quantity.value,"sizes":["S","M","L","XL"] }
+  window.products.push(obj);
   importDataToLocalStorage();
 };
--- JavaScripts/MainDB.js
+++ JavaScripts/MainDB.js
@@ -10,15 +10,276 @@ window.accounts = [
 ];
 
 window.products = [
-  {"id": 1,"productName": "vay","image": "../images/products/id001.jpg","price": 60000,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 2,"productName": "quan","image": "../images/products/id002.jpg","price": 20000,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 3,"productName": "ao","image": "../images/products/id003.jpg","price": 0,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 4,"productName": "d","image": "../images/products/id004.jpg","price": 0,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 5,"productName": "ao","image": "../images/products/id005.jpg","price": 0,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 6,"productName": "a","image": "../images/products/id005.jpg","price": 0,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 7,"productName": "bb","image": "../images/products/id007.jpg","price": 0,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 8,"productName": "c","image": "../images/products/id001.jpg","price": 0,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]},
-  {"id": 9,"productName": "ac","image": "../images/products/id001.jpg","price": 0,"category": "Ao", "amount": 100,"sizes": ["S","M","L","XL"]}
+  {
+      "id": 1,
+      "productName": "Dumb Heart Tee - Black",
+      "image": "../images/products/id001.jpg",
+      "price": 220000,
+      "category": "Tee",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 2,
+      "productName": "Outliner Tee - White",
+      "image": "../images/products/id002.jpg",
+      "price": 220000,
+      "category": "Tee",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 3,
+      "productName": "Basic Sticker & Logo Tee - Black",
+      "image": "../images/products/id003.jpg",
+      "price": 220000,
+      "category": "Tee",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 4,
+      "productName": "Embroidery Logo Tee V1.0 - White",
+      "image": "../images/products/id004.jpg",
+      "price": 220000,
+      "category": "Tee",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 5,
+      "productName": "Basic Polo V1.0 - White",
+      "image": "../images/products/id005.jpg",
+      "price": 220000,
+      "category": "Polo",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 6,
+      "productName": "Pink World Baby Tee",
+      "image": "../images/products/id006.jpg",
+      "price": 220000,
+      "category": "Tee",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 7,
+      "productName": "Mixer Boxy Tee",
+      "image": "../images/products/id007.jpg",
+      "price": 220000,
+      "category": "Tee",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 8,
+      "productName": "Basic Shirt - Black",
+      "image": "../images/products/id008.jpg",
+      "price": 250000,
+      "category": "Shirt",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 9,
+      "productName": "Short-sleeve Oxford Shirt - Pink",
+      "image": "../images/products/id009.jpg",
+      "price": 250000,
+      "category": "Shirt",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 10,
+      "productName": "Basic Shirt - White",
+      "image": "../images/products/id010.jpg",
+      "price": 250000,
+      "category": "Shirt",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 11,
+      "productName": "Basic Polo V1.0 - Black",
+      "image": "../images/products/id011.jpg",
+      "price": 250000,
+      "category": "Polo",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 12,
+      "productName": "Line Track Jacket",
+      "image": "../images/products/id012.jpg",
+      "price": 380000,
+      "category": "Jacket",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 13,
+      "productName": "Short-sleeve Oxford Shirt - Blue",
+      "image": "../images/products/id013.jpg",
+      "price": 250000,
+      "category": "Shirt",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 14,
+      "productName": "BIG LOGO HOODIE - Dark Gray",
+      "image": "../images/products/id014.jpg",
+      "price": 420000,
+      "category": "Hoodie",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 15,
+      "productName": "SIGNATURE HOODIE - Light Gray",
+      "image": "../images/products/id015.jpg",
+      "price": 420000,
+      "category": "Hoodie",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 16,
+      "productName": "BIG LOGO HOODIE - Dark Green",
+      "image": "../images/products/id016.jpg",
+      "price": 420000,
+      "category": "Hoodie",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 17,
+      "productName": "SIGNATURE HOODIE - Black",
+      "image": "../images/products/id017.jpg",
+      "price": 420000,
+      "category": "Hoodie",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 18,
+      "productName": "Basic Kaki Short",
+      "image": "../images/products/id018.jpg",
+      "price": 180000,
+      "category": "Short",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 19,
+      "productName": "Line Track Jacket -Black",
+      "image": "../images/products/id019.jpg",
+      "price": 380000,
+      "category": "Jacket",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 20,
+      "productName": "Metal Dress Pants - Black",
+      "image": "../images/products/id020.jpg",
+      "price": 280000,
+      "category": "Long-pant",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 21,
+      "productName": "Hindless Track Pants - Light Gray ",
+      "image": "../images/products/id021.jpg",
+      "price": 280000,
+      "category": "Long-pant",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 22,
+      "productName": "Hindless Track Pants - Blackac",
+      "image": "../images/products/id022.jpg",
+      "price": 280000,
+      "category": "Long-pant",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 23,
+      "productName": "Hindless Track Pants - White",
+      "image": "../images/products/id023.jpg",
+      "price": 280000,
+      "category": "Long-pant",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 24,
+      "productName": "Basic Short SS2 - Black",
+      "image": "../images/products/id024.jpg",
+      "price": 180000,
+      "category": "Short",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 25,
+      "productName": "Basic Short SS2 - Gray",
+      "image": "../images/products/id025.jpg",
+      "price": 180000,
+      "category": "Short",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 26,
+      "productName": "Minimal Zipper Hoodie - Black",
+      "image": "../images/products/id026.jpg",
+      "price": 420000,
+      "category": "Hoodie",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 27,
+      "productName": "Minimal Zipper Hoodie - Gray",
+      "image": "../images/products/id027.jpg",
+      "price": 420000,
+      "category": "Hoodie",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 28,
+      "productName": "BIG LOGO HOODIE - Pink",
+      "image": "../images/products/id028.jpg",
+      "price": 420000,
+      "category": "Hoodie",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 29,
+      "productName": "Basic Bucket - Black",
+      "image": "../images/products/id029.jpg",
+      "price": 80000,
+      "category": "Hat",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  },
+  {
+      "id": 30,
+      "productName": "Mũ lưỡi trai",
+      "image": "../images/products/id030.jpg",
+      "price": 80000,
+      "category": "Hat",
+      "amount": 0,
+      "sizes": ["S", "M", "L", "XL"]
+  }
 ];
 
 window.addToCarts = [
